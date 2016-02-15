4cdd1013f66dee3e6faf9544651770aff9484e45
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-02-15 11:47:36 -0600

    placeholder finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react"
+import Placeholder from "./placeholder"
 
 export default class App extends Component {
   render() {
-    return <h1>Hello World</h1>
+	  return (
+       <div>
+	       <Placeholder placeholder="First name" />
+	       <Placeholder placeholder="Last name" />
+       </div>)
   }
 }
---
+++ src/components/placeholder.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react"
+
+export default class Placeholder extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {text: ''};
+  this.handleInputChange = this.handleInputChange.bind(this); 
+ }      
+
+ handleInputChange(e) {
+  let newText = e.target.value;
+  this.setState({text: newText});
+ }
+
+ render() {
+   let spanClass = (this.state.text) ? "stamp" : "placeholder";
+   return(
+     <div className="fancy-input">
+       <input type="text" value={this.state.text} onChange={this.handleInputChange} />
+       <span className={spanClass}>{this.props.placeholder}</span>
+     </div>
+   )
+ }
+}
